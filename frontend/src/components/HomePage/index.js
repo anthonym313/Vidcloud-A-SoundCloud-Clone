30814fcaad0238d71f0019724802ace79f877d38
Anthony <anthony.a.s.martin@gmail.com>
2021-06-23 13:49:49 -0400

    hero styling complete

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -5,6 +5,7 @@ export default function HomePage(){
         <div className="homepage_container">
             <div className="homepage_banner_container">
                 <div className="homepage_banner_text"> Discover more with VidCloud GO+
+                    <p>Upload your first video and begin your journey.</p>
                 </div>  
                 <div className="homepage_banner_photo" id='video'>
                     <video src="https://assets.mixkit.co/videos/preview/mixkit-sunset-seen-at-sea-from-a-coast-aerial-shot-38859-large.mp4" autoPlay loop muted height="450px" width="auto"/>
