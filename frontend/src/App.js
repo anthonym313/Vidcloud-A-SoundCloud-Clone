f79296dee532275e5dee0e87c0c3c59e66afc119
Anthony <anthony.a.s.martin@gmail.com>
2021-06-23 13:09:28 -0400

    video hero centered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import HomePage from "./components/HomePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,14 +18,11 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
           <Route path="/signup">
             <SignupFormPage />
-          </Route>
-          <Route path='users/:id'>
-            {/* <UserProfilePage/> */}
-          </Route>
-          <Route path='videos/:id'>
-
           </Route>
         </Switch>
       )}
---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.css'
+export default function HomePage(){
+    return (
+        <div className="homepage_container">
+            <div className="homepage_banner_container">
+                <div className="homepage_banner_photo" id='video'>
+                    <video src="https://assets.mixkit.co/videos/preview/mixkit-sunset-seen-at-sea-from-a-coast-aerial-shot-38859-large.mp4" autoPlay loop muted height="450px" width="auto"/>
+                    <div className="homepage_banner_text">
+                        Discover more with VidCloud GO+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
