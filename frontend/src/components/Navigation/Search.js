57b272f1143b1c0a602b8f55a989412402bd0f17
Anthony <anthony.a.s.martin@gmail.com>
2021-06-23 17:40:59 -0400

    search return good results

--- frontend/src/components/Navigation/Search.js
+++ frontend/src/components/Navigation/Search.js
@@ -4,20 +4,20 @@ import './Navigation.css'
 export default function Search() {
     const [searchTerm, setSearchTerm] = useState('');
     
-    const apiKey = "";
+    
     const submitHelper= async (searchTerm)=>{
-        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&type=video&key=${apiKey}`)
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&type=video&key=AIzaSyBUUrsqNO-jsPXU5K8fPnVqFVYlZed3qfE`)
         const result = await response.json()
         console.log(result)
     }
     const handleSubmit = (event) =>{
         event.preventDefault()
-        submitHelper(event.target.value)
+        submitHelper(searchTerm)
     }
     return (
         <>
             <form className='nav-search' onSubmit={handleSubmit}>
-                <input onChange={()=>setSearchTerm(searchTerm)} type='search' placeholder='Search A Video ...' autoComplete='off' className="search_input" /> 
+                <input onChange={(e)=>setSearchTerm(e.target.value)} value={searchTerm} type='search' placeholder='Search A Video ...' autoComplete='off' className="search_input" /> 
                 <button className='nav-search__button' type='submit'>Go</button>
             </form>
             
