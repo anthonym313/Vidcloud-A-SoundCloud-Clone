e2926cab782d5a637541b06d02063a56c2d45e73
Anthony <anthony.a.s.martin@gmail.com>
2021-06-23 14:41:55 -0400

    start trending box

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -8,9 +8,25 @@ export default function HomePage(){
                     <p>Upload your first video and begin your journey.</p>
                 </div>  
                 <div className="homepage_banner_photo" id='video'>
-                    <video src="https://assets.mixkit.co/videos/preview/mixkit-sunset-seen-at-sea-from-a-coast-aerial-shot-38859-large.mp4" autoPlay loop muted height="450px" width="auto"/>
+                    <video src="https://assets.mixkit.co/videos/preview/mixkit-sunset-seen-at-sea-from-a-coast-aerial-shot-38859-large.mp4" autoPlay loop muted height="450" width="auto"/>
                 </div>
             </div>
+            <div className="trending_video_container">
+                <div className="trending_video_header">See what's been trending for free in the VidCloud community</div>
+                <div className="trending_gallery_container">
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                    <div className="trending_video"></div>
+                </div>
+            </div>
+
         </div>
     )
 }
