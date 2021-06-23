7e1310809b7ee3e4540cf845fc4bcea285e2f630
Anthony <anthony.a.s.martin@gmail.com>
2021-06-23 16:09:33 -0400

    homepage layout

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -14,16 +14,18 @@ export default function HomePage(){
             <div className="trending_video_container">
                 <div className="trending_video_header">See what's been trending for free in the VidCloud community</div>
                 <div className="trending_gallery_container">
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
-                    <div className="trending_video"></div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                    <div className="trending_video">thumbnail</div>
+                   
+                 
                 </div>
             </div>
 
